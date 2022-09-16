61efb4f39d776bd761a2f684071d182036af3218
Crowdbotics <team@crowdbotics.com>
2022-09-16 21:50:48 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "kirby_racing_37081",
+  url: "https://kirby_racing_37081.botics.co",
+  api: "https://kirby_racing_37081.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
